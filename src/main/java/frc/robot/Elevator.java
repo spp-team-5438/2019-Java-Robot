6e4727000c82ad54f8fc9b416e2fec760167cb30
Samuel Leonte <samgleonte@gmail.com>
2019-02-05 20:46:17 -0500

    Fix all errors

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -8,36 +8,33 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import com.wpi.first.wpilibj.XboxController;
-import com.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID;
 
 public class Elevator {
     //define speedcontrollers for victors
-    public static SpeedController emotor1 = new WPI_VictorSPX(5);
-    public static SpeedController emotor2 = new WPI_VictorSPX(6);
+    public static WPI_VictorSPX emotor1 = new WPI_VictorSPX(5);
+    public static WPI_VictorSPX emotor2 = new WPI_VictorSPX(6);
 
     public void teleopPeriodic() {
         //set the second motor to follow the first motor so they both perform the same action
         emotor2.follow(emotor1);
 
-        //make sure the motors don't spin too fast
-        emotor1.setMaxOutput(0.5);
-        emotor2.setMaxOutput(0.5);
-
         //get and set the boolean values for if the bumpers are pressed or not
-        public boolean rightBumperPressed = getBumperPressed(GenericHID.righthand);
-        public boolean leftBumperPressed = getBumperPressed(GenericHID.leftHand);
+        public boolean rightBumperPressed = getBumperPressed(rightHand);
+        public boolean leftBumperPressed = getBumperPressed(leftHand);
 
         //scan for bumpers being pressed to drive the motors
         //scan for bumpers not being pressed to stop the motors
-        if (rightBumperPressed == true) {
-          emotor2.set(ControlMode.PercentOutput, 0.5);
-        } else if (rightBumperPressed == false) {
-          emotor2.set(ControlMode.PercentOutput, 0);
-        } else if (leftBumperPressed == true) {
-          emotor2.set(ControlMode.PercentOutput, -0.5);
-        } (else if leftBumperPressed == false) {
-          emotor2.set(ControlMode.PercentOutput, 0);
+        if (rightBumperPressed = true) {
+          emotor2.set(0.5);
+        } else if (rightBumperPressed = false) {
+          emotor2.set(0);
+        } else if (leftBumperPressed = true) {
+          emotor2.set(-0.5);
+        } else if (leftBumperPressed = false) {
+          emotor2.set(0);
         }
     }
 }
