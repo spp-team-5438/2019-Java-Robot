3b606f90f593b46d58a2f086f5099efd6b2f2eb9
Samuel Leonte <samgleonte@gmail.com>
2019-02-08 23:39:23 -0500

    seperate drivetrain code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,11 +8,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.XboxController;
 
+import frc.robot.Drivetrain;
 import frc.robot.Pneumatics;
 import frc.robot.Arm;
 import frc.robot.Camera;
@@ -22,28 +20,14 @@ import frc.robot.REVDigitBoard;
 public class Robot extends TimedRobot {
 
   //define all other classes
+  Drivetrain mecanumDrivetrain = new Drivetrain();
   Pneumatics pneumatics = new Pneumatics();
   Arm arm = new Arm();
   Camera camera = new Camera();
   Elevator elevator = new Elevator();
   REVDigitBoard display = new REVDigitBoard();
 
-  //define numbers of talons
-  public static int frontLeftnum = 2;
-  public static int rearLeftnum = 1;
-  public static int frontRightnum = 4;
-  public static int rearRightnum = 3;
-
-  //create speedcontroller entries for talons
-  public static SpeedController frontLeft = new WPI_TalonSRX(frontLeftnum);
-  public static SpeedController rearLeft = new WPI_TalonSRX(rearLeftnum);
-  public static SpeedController frontRight = new WPI_TalonSRX(frontRightnum);
-  public static SpeedController rearRight = new WPI_TalonSRX(rearRightnum);
-
-  //create mecanum drive
-  public static MecanumDrive mDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
-
-  //define xbox controller for use with mecanum drive
+  //define xbox controller
   public static XboxController controller = new XboxController(0);
 
   //run when the robot is starting up; initialization code is placed here:
@@ -61,17 +45,11 @@ public class Robot extends TimedRobot {
   //run periodically when the operator is in control:
   @Override
 	public void teleopPeriodic() {
-    
     //call other classes
+    mecanumDrivetrain.main();
     pneumatics.main();
     arm.main();
     elevator.main();
-    
-    //create drivetrain with controller inputs and set safety
-    mDrive.driveCartesian((controller.getRawAxis(0) * 1), (controller.getRawAxis(1) * -1), (controller.getRawAxis(4) * 1));
-    mDrive.setSafetyEnabled(true);
-    mDrive.setExpiration(0.1);
-    mDrive.setDeadband(0.1);
 }
 
   //run when robot enters autonomous mode; initializtion for autonomous should be placed here:
@@ -88,5 +66,4 @@ public class Robot extends TimedRobot {
   @Override
 	public void disabledInit() {
   }
-
 }
