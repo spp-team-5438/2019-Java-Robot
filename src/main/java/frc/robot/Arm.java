7cda4d73f4c3df6e2c1a87b68cc8cc97872b71b1
Samuel Leonte <samgleonte@gmail.com>
2019-02-19 12:03:05 -0500

    fixes

--- src/main/java/frc/robot/Arm.java
+++ src/main/java/frc/robot/Arm.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Arm {
 
@@ -30,19 +31,31 @@ public class Arm {
         
         //set motors safety
         armMotor.setSafetyEnabled(true);
-        armMotor.setExpiration(0.05);
+        armMotor.setExpiration(0.020);
+
+        //send values to smartdashbaord
+        double armMotorSpeed = armMotor.getSpeed();
+        SmartDashboard.putNumber("Arm Speed", armMotorSpeed);
 
         //if right trigger is pressed spin one way, if left is pressed spin other way
         if (rightTriggerValue > 0) {
             armMotor.setInverted(true);
             armMotor.set(rightTriggerValue);
+            System.out.println("ARM - INTAKE " + armMotorSpeed);
+            if (rightTriggerValue > 0.5) {
+                armMotor.set(0.5);
+            }
         }
         else if (leftTriggerValue > 0) {
             armMotor.setInverted(false);
             armMotor.set(leftTriggerValue);
-        }
+            System.out.println("ARM - EXPEL " + armMotorSpeed);
+            if (leftTriggerValue > 0.5) {
+                armMotor.set(0.5);
+            }
         else {
             armMotor.set(0);
         }
+    }
     }
 }
